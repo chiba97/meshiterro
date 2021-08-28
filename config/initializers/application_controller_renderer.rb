f343f5d46b6e43be02830316bdb449f171e77a6e
chiba97 <ktfkyrya@gmail.com>
2021-08-28 03:28:20 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c5d8333c2c7b588208e1b84503c41548b4e88cf26557b58d16bea0090aaa48cf880126112e9e521345a630ac00f93dbdf699ad22048c9ee48cdbae071e68a149'
